dda43aba18c638dfaa873e39ead71303850a23f3
alvaro270 <alvarogamer270@gmail.com>
2022-06-08 21:06:27 -0500

    lab12

---
+++ src/router/routes.jsx
@@ -0,0 +1,8 @@
+// Rutas de mi aplicacion
+
+export const HOME = "/";
+
+// ! Recurden que el nombre debe estar acompañado de :
+export const ABOUT = "/about/:slug";
+
+export const ADMIN = "/admin";
